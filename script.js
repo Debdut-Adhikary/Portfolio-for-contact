2d5c13c16c9ae7be14c1a08fdd63d6c6e5a40273
Debdut-Adhikary <debdutadhikary469@gmail.com>
2026-04-20 23:57:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Contact Form
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message sent successfully!");
+});
